74666617d58a25885993747013d408d0b5cc8a2e
siverNa <69504543+siverNa@users.noreply.github.com>
2020-11-15 21:28:54 +0900

    [solve] 백준#2558 - A+B - 2
    알고리즘 A+B -2 코드

---
+++ sangguNa/A+B-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //int형 변수 a,b 생성
+    int a;
+    int b;
+
+    //a, b에 입력받은 값 저장하고 a+b 출력
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("%d", a+b);
+}
