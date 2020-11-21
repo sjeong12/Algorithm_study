8113b9abe840275e09853e839a6df08eed364801
siverNa <69504543+siverNa@users.noreply.github.com>
2020-11-21 10:15:46 +0900

    [solve] 백준#10952 - A+B - 5
    A+B-5 문제 풀이

---
+++ sangguNa/10952_A+B-5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a; //사용자로부터 입력받은 값을 저장할 변수 a, b 생성
+    int b;
+
+    while(1) //마지막에 0, 0이 입력될 때까지 입력을 계속 받음
+    {
+        scanf("%d %d", &a, &b);
+        
+        if(a == 0 && b == 0) //만약 0, 0 을 입력받으면 프로그램을 종료함
+            return (0);
+        else if((a > 0 && a < 10) && (b > 0 && b < 10)) //0 < a, b < 10이면 덧셈을 수행함
+            printf("%d\n", a + b);
+        else //그게 아니라면 조건에 충족하지 않는다고 오류문을 출력함.
+            printf("not enought condition\n");
+        
+    }
+    return (0);
+}
