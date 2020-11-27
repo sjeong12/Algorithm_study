1cba3d22591c515057d676b896299ef5fd6211d4
siverNa <69504543+siverNa@users.noreply.github.com>
2020-11-27 20:17:55 +0900

    개인적인 c++ 공부
    기존의 c언어에서 쓰는 문법이 c++에도 적용이 가능하단 것을 확인

---
+++ sangguNa/plus_cpp/1_3_예제.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(void)
+{
+    int a;
+    int b;
+    
+    std::cout << "insert number" << std::endl;
+    std::cin >> a;
+    std::cin >> b;
+
+    std::cout << "your insert number is " << a << " and " << b << std::endl;
+
+    return (0);
+}
