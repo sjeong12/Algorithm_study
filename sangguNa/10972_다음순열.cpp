9b289be693654b61dd656dfe0a86677a42395446
siverNa <69504543+siverNa@users.noreply.github.com>
2020-12-15 14:51:34 +0900

    [solve] 백준#10972 - 다음순열
    다음순열 문제 풀이
    참고 :
    https://xtar.tistory.com/m/65?category=780053
    https://jaimemin.tistory.com/1061

---
+++ sangguNa/10972_다음순열.cpp
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <functional>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+    int num[10000], n; //문제 조건이 1 <= n <= 10,000 이므로 배열의 크기를 10,000으로 설정
+    int index = 0; 
+    int tmp; //서로 바꾸기 위한 변수
+
+    scanf("%d", &n); //사용자로부터 자릿수를 입력받음
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &num[i]); //n만큼 순열을 입력받는다
+    
+    for(int i = n - 1; i > 0; i--)
+    {
+       if(num[i - 1] < num[i])
+       {
+           index = i;
+           break;
+       }
+    }
+
+    if(index == 0)
+    {
+        printf("-1\n");
+        return (0);
+    }
+
+    for(int i = n - 1; i >= index; i--)
+    {
+        if(num[index - 1] < num[i])
+        {
+            tmp = num[i];
+            num[i] = num[index - 1];
+            num[index -1] = tmp;
+            break;
+        }
+    }
+    //algorithm 헤더파일에 속해있는 sort 알고리즘
+    //sort(start, end)를 이용하여 start ~ end 의 범위에 있는 인자를 오름차순으로 정렬
+    //마지막 3번째 인자에 'greater<자료형>()'을 사용하면 내림차순
+    //' less<자료형>()'을 사용하면 오름차순(기본값)
+    sort(num + index, num + n); 
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", num[i]);
+    
+    
+    return (0);
+}
