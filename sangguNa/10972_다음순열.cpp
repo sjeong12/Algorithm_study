7dcefe4dd3377306c23ec72226bbddb452596f83
siverNa <69504543+siverNa@users.noreply.github.com>
2020-12-16 13:58:30 +0900

    [solve] 백준#11066 - 파일합치기, [fix] 백준#10972 - 다음 순열
    파일합치기 문제풀이
    참고 :
    https://kyunstudio.tistory.com/75
    https://js1jj2sk3.tistory.com/search/11066
    https://kevink1113.tistory.com/entry/C-%EB%B0%B1%EC%A4%80-11066-%ED%8C%8C%EC%9D%BC-%ED%95%A9%EC%B9%98%EA%B8%B0

--- sangguNa/10972_다음순열.cpp
+++ sangguNa/10972_다음순열.cpp
@@ -6,46 +6,42 @@ using namespace std;
 int main(void)
 {
     int num[10000], n; //문제 조건이 1 <= n <= 10,000 이므로 배열의 크기를 10,000으로 설정
-    int index = 0; 
+    int i, j; //반복에 사용할 변수
     int tmp; //서로 바꾸기 위한 변수
 
     scanf("%d", &n); //사용자로부터 자릿수를 입력받음
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         scanf("%d", &num[i]); //n만큼 순열을 입력받는다
     
-    for(int i = n - 1; i > 0; i--)
+    for(i = n - 1; i > 0; i--)
     {
-       if(num[i - 1] < num[i])
-       {
-           index = i;
-           break;
-       }
+        if(num[i - 1] < num[i]) //맨 뒤(i == n - 1)부터 탐색하여 오름차순이 아닌 지점(i - 1)을 확인함
+        {
+            for(j = n - 1; j > i - 1; j--) //찾아낸 지점(i - 1)보다 큰 수를 뒤에서부터 찾아 서로 바꿈
+            {
+                if(num[i - 1] < num[j])
+                    break;
+            }
+            tmp = num[i - 1];
+            num[i - 1] = num[j];
+            num[j] = tmp;
+            break;
+        }
     }
 
-    if(index == 0)
+    if(i == 0)
     {
         printf("-1\n");
         return (0);
-    }
-
-    for(int i = n - 1; i >= index; i--)
-    {
-        if(num[index - 1] < num[i])
-        {
-            tmp = num[i];
-            num[i] = num[index - 1];
-            num[index -1] = tmp;
-            break;
-        }
     }
     //algorithm 헤더파일에 속해있는 sort 알고리즘
     //sort(start, end)를 이용하여 start ~ end 의 범위에 있는 인자를 오름차순으로 정렬
     //마지막 3번째 인자에 'greater<자료형>()'을 사용하면 내림차순
     //' less<자료형>()'을 사용하면 오름차순(기본값)
-    sort(num + index, num + n); 
+    sort(num + i, num + n); 
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         printf("%d ", num[i]);
     
     
