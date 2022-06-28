c62b3a73b2efad40bd8625f97de02f058f9f562b
siverNa <69504543+siverNa@users.noreply.github.com>
2022-06-28 21:17:36 +0900

    [solve] 백준#3046 - R2
    R2 문제 풀이
    참고 :
    혼자 해결

---
+++ sangguNa/3046_R2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int R1, R2, S;
+	int temp = 0;
+
+	cin >> R1 >> S;
+	temp = S * 2;
+	R2 = temp - R1;
+
+	cout << R2 << '\n';
+	return (0);
+}
